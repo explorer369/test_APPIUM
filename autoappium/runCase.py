3e59b5780bd389eed64f95b163a7b8b5bd31f3d1
explorer369 <zzf198987@qq.com>
2016-07-03 20:47:57 +0800

    更新说明
    1.增加导入find模块时就执行文件夹删除新建功能。
    2.修改中文报告标题。
    3.解决LOG日志递增重复打印问题。
    4.新增python方式结束APPIUM服务，取消用BAT命令

--- autoappium/runCase.py
+++ autoappium/runCase.py
@@ -10,7 +10,7 @@ from find import *
 #logger = log.Logger('rst/log.log',clevel = logging.DEBUG,Flevel = logging.INFO)
 def load_tests():
     #logger加到此处eclispe才有日志输出加到这里也可以
-    logger = log.Logger('rst/log.log',clevel = logging.DEBUG,Flevel = logging.INFO)
+ #   logger = log.Logger('rst/log.log',clevel = logging.INFO,Flevel = logging.INFO)
     test_file_strings = glob.glob('test_*.py')    
     module_strings = [str[0:len(str)-3] for str in test_file_strings]
     print module_strings
@@ -22,18 +22,19 @@ def load_tests():
   #  print logger.info(test_file_strings)
 if __name__ == '__main__':    
     if finddevices() == 'yes': # 检查是否已连接上手机
-        cleanEnv()
-        start_Appium('localhost','4723','4780','rst/appium.log')# 启动APPIUP服务   
+#        cleanEnv() #让导入find模块时就让执行了此步操作
+        start_Appium('localhost','4723','4780','rst/appium.log')# 启动APPIUP服务 
         #logger加到此处eclispe才有日志输出
         #logger = log.Logger('rst/log.log',clevel = logging.DEBUG,Flevel = logging.INFO)
         timestr = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
         filename ='result_'+ timestr + '.html'
         print filename
         fp =open(filename,'wb')
-        runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title='TEE',description='AAA')
-    #    runner.run(load_tests())
+        runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'自动化测试报告',description=u'报告明细')
+        runner.run(load_tests())
         #下面这句是原始的，不知道是什么意思，经过上面的改造，目前可以正常使用，且有报HTML报告输出
-        unittest.TextTestRunner(verbosity=2).run(load_tests()) #
+    #    unittest.TextTestRunner(verbosity=2).run(load_tests()) #
         fp.close()
+        logging.shutdown()
         # 结束APPIUM服务
         stop_Appium('http://localhost:4723/wd/hub')
